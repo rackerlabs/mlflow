afcaf412d0d4d6faa75cc46aa7c7977e12c89ebf
Jerry George <jgeorge@onica.com>
2020-03-24 13:02:39 -0400

    Add batch transform functionality (WIP)

--- mlflow/projects/__init__.py
+++ mlflow/projects/__init__.py
@@ -759,6 +759,18 @@ def _create_run(uri, experiment_id, work_dir, entry_point):
     return active_run
 
 
+def _get_run_env_vars_from_context():
+    """
+    Returns a dictionary of environment variable key-value pairs to set in subprocess launched
+    to run MLflow projects.
+    """
+    return {
+        tracking._RUN_ID_ENV_VAR: os.environ.get(tracking._RUN_ID_ENV_VAR, None),
+        tracking._TRACKING_URI_ENV_VAR: tracking.get_tracking_uri(),
+        tracking._EXPERIMENT_ID_ENV_VAR: os.environ.get(tracking._EXPERIMENT_ID_ENV_VAR, None),
+    }
+
+
 def _get_run_env_vars(run_id, experiment_id):
     """
     Returns a dictionary of environment variable key-value pairs to set in subprocess launched
